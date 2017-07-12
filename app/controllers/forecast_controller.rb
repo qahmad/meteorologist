a1eb046762584c56aa2f6680dcf910c9f84f41a1
Saud <qahmad@chicagobooth.edu>
2017-07-12 05:00:01 +0000

    First pass

--- app/controllers/forecast_controller.rb
+++ app/controllers/forecast_controller.rb
@@ -17,16 +17,19 @@ class ForecastController < ApplicationController
     # ==========================================================================
 
 
+# https://api.darksky.net/forecast/a4f569fa6c8872b6f37f4c6178f8d120/41.7891,-87.5954
 
-    @current_temperature = "Replace this string with your answer."
+    parsed_data = JSON.parse(open("https://api.darksky.net/forecast/a4f569fa6c8872b6f37f4c6178f8d120/" + @lat + "," + @lng).read)
 
-    @current_summary = "Replace this string with your answer."
+    @current_temperature = parsed_data["currently"]["temperature"]
 
-    @summary_of_next_sixty_minutes = "Replace this string with your answer."
+    @current_summary = parsed_data["currently"]["summary"]
 
-    @summary_of_next_several_hours = "Replace this string with your answer."
+    @summary_of_next_sixty_minutes = parsed_data["minutely"]["summary"]
 
-    @summary_of_next_several_days = "Replace this string with your answer."
+    @summary_of_next_several_hours = parsed_data["hourly"]["summary"]
+
+    @summary_of_next_several_days = parsed_data["daily"]["summary"]
 
     render("forecast/coords_to_weather.html.erb")
   end
